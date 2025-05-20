ef04bc6bf3c23e4cd9a7d3605f8d1d3bbc5f7cf8
Adarsh <adarshreddy2106@gmail.com>
2025-05-20 20:04:51 +0530

    Using ejs as Templating Engine

--- app.js
+++ app.js
@@ -2,15 +2,11 @@ const path = require('path'); // import path module
 const express = require('express'); // import express
 // const routes = require('./routes/admin'); // import the routes
 const bodyParser = require('body-parser'); // import body-parser
-const expressHbs = require('express-handlebars'); // import express-handlebars
+
 
 const app = express(); // create an instance of express
 
-app.engine('hbs', expressHbs({
-    layoutsDir: 'views/layouts', 
-    defaultLayout: 'mainlayout', 
-    extname: 'hbs'})); // set up handlebars as the view engine
-app.set('view engine', 'hbs'); // set the view engine to handlebars
+app.set('view engine', 'ejs'); // set the view engine to handlebars
 app.set('views', 'views'); // set the views directory
 
 const admindata = require('./routes/admin'); // import the admin routes
@@ -23,8 +19,10 @@ app.use('/admin', admindata.routes); // use the admin routes for any request tha
 app.use(ShopRoutes); // use the shop routes for any request that starts with /shop
 
 app.use((req, res, next) => { // middleware to handle 404 errors
-    // res.status(404).sendFile(path.join(__dirname, 'views','error.html')); // send a 404 response
-    res.render('error', {PageTitle:'Page Not Found'}); // render the error view
+    res.status(404).render('error', {
+        PageTitle: 'Page Not Found',
+        path: req.url // or simply '' if you don't want any menu item active
+    });
 }); // end of middleware 
 
-app.listen(2005); // start the server on port 2005 
+app.listen(2005); // start the server on port 2005
